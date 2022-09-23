13d3b14b42fca84ec2035741946ac25f716b6421
yangxinxiang <yangxinxiang@100tal.com>
2022-09-23 19:31:53 +0800

    feat: 用vue2 cli项目测试加载模块文件

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import yuv2,  {FrameSink as FrameSink2, SoftwareFrameSink as SoftwareFrameSink2}  from './lib/yuv-canvas.js'
-import yuv3,  {FrameSink as FrameSink3, SoftwareFrameSink as SoftwareFrameSink3}  from '../node_modules/yuv-canvas/src/yuv-canvas22'
+
 import yuv1, {FrameSink as FrameSink1, SoftwareFrameSink as SoftwareFrameSink1, WebGLFrameSink as WebGLFrameSink1, attach} from 'yuv-canvas';
 
 
@@ -33,10 +33,6 @@ export default {
       console.log('startTest :: 从单独文件导入:: yuv2 = ', yuv2)
       console.log('startTest :: 从单独文件导入:: FrameSink2 = ', FrameSink2)
       console.log('startTest :: 从单独文件导入:: SoftwareFrameSink2 = ', SoftwareFrameSink2)
-      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~')
-      console.log('startTest :: 从单独文件导入:: yuv3 = ', yuv3)
-      console.log('startTest :: 从单独文件导入:: FrameSink3 = ', FrameSink3)
-      console.log('startTest :: 从单独文件导入:: SoftwareFrameSink3 = ', SoftwareFrameSink3)
     }
   }
 }
